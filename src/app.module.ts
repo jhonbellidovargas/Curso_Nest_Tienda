0e458f42fa9b0ca802d00d05f2a9425c27fb9f72
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-03-21 11:19:50 -0500

    fix errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
-import { OrdersController } from './controllers/orders.controller';
-import { UsersController } from './controllers/users.controller';
-//import { CustomersController } from './controllers/customers.controller';
 import { BrandsController } from './controllers/brands.controller';
+import { CustomerController } from './controllers/customers.controller';
+import { UsersController } from './controllers/users.controller';
 import { ProductsService } from './services/products.service';
+import { CategoriesService } from './services/categories.service';
+import { BrandsService } from './services/brands.service';
+import { CustomersService } from './services/customers.service';
+import { UsersService } from './services/users.service';
 
 @Module({
   imports: [],
@@ -15,11 +18,17 @@ import { ProductsService } from './services/products.service';
     AppController,
     ProductsController,
     CategoriesController,
-    OrdersController,
-    UsersController,
-    //CustomersController,
     BrandsController,
+    CustomerController,
+    UsersController,
   ],
-  providers: [AppService, ProductsService],
+  providers: [
+    AppService,
+    ProductsService,
+    CategoriesService,
+    BrandsService,
+    CustomersService,
+    UsersService,
+  ],
 })
 export class AppModule {}
